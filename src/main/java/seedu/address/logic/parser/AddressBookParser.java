8271082fbd94f1786d6be3d801dfa511a05cb991
mannggoo <yuhuilim1999@gmail.com>
2019-10-06 16:45:29 +0800

    Edit Parser

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -7,13 +7,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.ClearCommand;
+//import seedu.address.logic.commands.ClearCommand;
+//import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ShowCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -24,7 +24,7 @@ public class AddressBookParser {
     /**
      * Used for initial separation of command word and args.
      */
-    private static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<commandWord>\\S+)(?<arguments>.*)");
+    private static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<commandWord>\\S+)(?<description>[^-]*)(?<flags>.*)");
 
     /**
      * Parses user input into command for execution.
@@ -40,26 +40,34 @@ public class AddressBookParser {
         }
 
         final String commandWord = matcher.group("commandWord");
-        final String arguments = matcher.group("arguments");
+        final String description = matcher.group("description");
+        final String flags = matcher.group("flags");
         switch (commandWord) {
 
-        case AddCommand.COMMAND_WORD:
-            return new AddCommandParser().parse(arguments);
+        case "task":
+            return new AddTaskCommandParser().parse(description, flags);
+
+        case "event":
+            return new AddEventCommandParser().parse(description, flags);
+
+        case "reminder":
+            return new AddReminderCommandParser().parse(description, flags);
 
         case EditCommand.COMMAND_WORD:
-            return new EditCommandParser().parse(arguments);
+            return new EditCommandParser().parse(description);
+            //return new EditCommandParser().parse(description, flags);
 
         case DeleteCommand.COMMAND_WORD:
-            return new DeleteCommandParser().parse(arguments);
+            return new DeleteCommandParser().parse(description);
 
-        case ClearCommand.COMMAND_WORD:
-            return new ClearCommand();
+        //case ClearCommand.COMMAND_WORD:
+        //    return new ClearCommand();
 
-        case FindCommand.COMMAND_WORD:
-            return new FindCommandParser().parse(arguments);
+        //case FindCommand.COMMAND_WORD:
+        //    return new FindCommandParser().parse(arguments);
 
-        case ListCommand.COMMAND_WORD:
-            return new ListCommand();
+        case ShowCommand.COMMAND_WORD:
+            return new ShowCommand();
 
         case ExitCommand.COMMAND_WORD:
             return new ExitCommand();
